59c069ae6d6519c7d2058dfc668616f9bda9fa72
krenol <lauritzgroeger@gmail.com>
2026-01-11 16:29:46 +0100

    Small bug fixes and deprecation migrations

--- backend/src/tests/models/api_error.test.ts
+++ backend/src/tests/models/api_error.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { ApiError } from '../../models/api/api_error';
 
 describe('ApiError Model', () => {
--- backend/src/tests/services/misc.test.ts
+++ backend/src/tests/services/misc.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { requestContainsParam, requestQueryContainsParam } from '../../services/misc';
 import { Request } from 'express';
 
