5016fc4e2b1c80e315f48e49b36b3d38cd4e121c
GregorLauritz <16196236+GregorLauritz@users.noreply.github.com>
2026-04-12 08:58:35 +0200

    Refactor motion detection logic and improve naming conventions in RPi and Ubuntu scripts

--- ansible/roles/setup_rpi/files/raspbian/main.py
+++ ansible/roles/setup_rpi/files/raspbian/main.py
@@ -2,27 +2,26 @@ import time
 import RPi.GPIO as GPIO
 from movement_detector import DisplayState, MotionSensor, ScreenToggle
 
-SENDSOR_PIN = 4  # define the sensorPin
+SENSOR_PIN = 4
 
 
 def loop():
     print("Start display state loop")
-    motionSensor = MotionSensor(SENDSOR_PIN)
-    screenToggle = ScreenToggle()
+    motion_sensor = MotionSensor(SENSOR_PIN)
+    screen_toggle = ScreenToggle()
     while True:
-        if motionSensor.multiMotionIsDetect(5):
+        if motion_sensor.multi_motion_is_detected(5):
             print("Turn display on")
-            screenToggle.setScreenState(DisplayState.on)
+            screen_toggle.set_screen_state(DisplayState.on)
         else:
             print("Turn display off")
-            screenToggle.setScreenState(DisplayState.off)
+            screen_toggle.set_screen_state(DisplayState.off)
         time.sleep(1)
 
 
-if __name__ == '__main__':  # Program start from here
+if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     try:
         loop()
-    # When 'Ctrl+C' is pressed, the child program destroy() will be executed.
     except KeyboardInterrupt:
         GPIO.cleanup()
--- ansible/roles/setup_rpi/files/ubuntu/main.py
+++ ansible/roles/setup_rpi/files/ubuntu/main.py
@@ -2,27 +2,26 @@ import time
 import lgpio
 from movement_detector import DisplayState, MotionSensor, ScreenToggle
 
-SENDSOR_PIN = 4  # define the sensorPin
-LGIO_CHIP = lgpio.gpiochip_open(0)
+SENSOR_PIN = 4
+GPIO_CHIP = lgpio.gpiochip_open(0)
 
 
 def loop():
     print("Start display state loop")
-    motionSensor = MotionSensor(SENDSOR_PIN, LGIO_CHIP)
-    screenToggle = ScreenToggle()
+    motion_sensor = MotionSensor(SENSOR_PIN, GPIO_CHIP)
+    screen_toggle = ScreenToggle()
     while True:
-        if motionSensor.multiMotionIsDetect(5):
+        if motion_sensor.multi_motion_is_detected(5):
             print("Turn display on")
-            screenToggle.setScreenState(DisplayState.on)
+            screen_toggle.set_screen_state(DisplayState.on)
         else:
             print("Turn display off")
-            screenToggle.setScreenState(DisplayState.off)
+            screen_toggle.set_screen_state(DisplayState.off)
         time.sleep(1)
 
 
-if __name__ == '__main__':  # Program start from here
+if __name__ == '__main__':
     try:
         loop()
-    # When 'Ctrl+C' is pressed, the child program destroy() will be executed.
     except KeyboardInterrupt:
-        lgpio.gpiochip_close(LGIO_CHIP)
+        lgpio.gpiochip_close(GPIO_CHIP)
